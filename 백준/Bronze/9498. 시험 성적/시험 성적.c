48778e5cdd9c83039bec1eeec4c48ba711b1b977
김가은 <128462535+Kgeaun@users.noreply.github.com>
2023-11-15 09:15:40 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d", &a);
+
+    switch(a) {
+        case 90 ...100:
+        printf("A");
+        break;
+        case 80 ...89 : 
+        printf("B");
+        break;
+        case 70 ...79 :
+        printf("C");
+        break;
+        case 60 ...69 :
+        printf("D");
+        break;
+        default:
+        printf("F");
+    }
+    return 0;
+}
