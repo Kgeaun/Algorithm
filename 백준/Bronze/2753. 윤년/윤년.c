89ad085e939c452ce25da932d359f522ed9a2e5b
김가은 <128462535+Kgeaun@users.noreply.github.com>
2023-12-06 17:08:03 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int num1;
+    scanf("%d", &num1);
+    if(num1 % 4 == 0 && num1 % 100 != 0) printf("1");
+    else if(num1 % 400 == 0) printf("1");
+    else printf("0");
+    return 0;
+}
