cb5369c22dccefe492943fc3ccc8979995bef24f
김가은 <128462535+Kgeaun@users.noreply.github.com>
2024-03-14 22:52:26 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int slice, int n) {
+        if(n % slice == 0) {
+           return n/slice;
+        }
+        else if(n % slice != 0) {
+           return n/slice+1;
+        }
+        return 0;
+    }
+}
