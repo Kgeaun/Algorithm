22ae2ba54ec6f32892f8941ae7e3dfbd73ee300b
김가은 <128462535+Kgeaun@users.noreply.github.com>
2024-04-03 20:06:58 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 103.76 ms, Memory: 27.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,21 @@
+'''
+return 공통 수가 제거된 후 남은 수들을 순서대로
+
+연속해서 들어오는 것만 
+
+arr = 배열 (숫자 0부터 9까 이루어짐)
+
+1) answer에 먼저 arr[0]을 append
+
+2) for문은 arr 만큼 돈다.
+2-1 if answer[i]과 answer[i-1]이 다르면 answer[i]를 append한다.
+
+'''
+def solution(arr):
+    answer = []
+    
+    answer.append(arr[0])
+    for i in range (1, len(arr)):
+        if arr[i] != arr[i-1]:
+            answer.append(arr[i])
+    return answer
