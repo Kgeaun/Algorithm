d758b7e11a34a8d9066cfcf1973bb6ac91d19429
김가은 <128462535+Kgeaun@users.noreply.github.com>
2024-04-07 15:59:48 +0900

    [Bronze III] Title: 학점계산, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,28 @@
+r = input()
+
+if r == "A+":
+    print(float(4.3))
+elif r == "A0":
+    print(float(4.0))
+elif r == "A-":
+    print(float(3.7))
+elif r == "B+":
+    print(float(3.3))
+elif r == "B0":
+    print(float(3.0))
+elif r == "B-":
+    print(float(2.7))
+elif r == "C+":
+    print(float(2.3))
+elif r == "C0":
+    print(float(2.0))
+elif r == "C-":
+    print(float(1.7))
+elif r == "D+":
+    print(float(1.3))
+elif r == "D0":
+    print(float(1.0))
+elif r == "D-":
+    print(float(0.7))
+elif r == "F":
+    print(float(0.0))
