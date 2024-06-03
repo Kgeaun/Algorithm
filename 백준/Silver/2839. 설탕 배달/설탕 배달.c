c7dbc1c7c454fd8e0fe897ff0e77bc71d97c40d2
김가은 <128462535+Kgeaun@users.noreply.github.com>
2024-06-03 20:09:55 +0900

    Update 설탕 배달.c

--- 백준/Silver/2839. 설탕 배달/설탕 배달.c
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.c
@@ -1,26 +1,20 @@
 #include <stdio.h>
 
-int main()
-{
-
+int main(){
 	int n; scanf("%d", &n);
 	int cnt = 0;
 
 	while (1)
 	{
-		if (n % 5 == 0)
-		{
+		if (n % 5 == 0){
 			cnt += n / 5;
 			break;
 		}
-		
 		n -= 3;
 		cnt++;
-
 		if (n <= 0) {
 			break;
 		}
-
 	}
 	if (n < 0) {
 		printf("-1");
