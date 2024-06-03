e19faa8b47a53009976a5e316bed7f07e2a44d60
김가은 <128462535+Kgeaun@users.noreply.github.com>
2024-06-03 20:09:01 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+
+	int n; scanf("%d", &n);
+	int cnt = 0;
+
+	while (1)
+	{
+		if (n % 5 == 0)
+		{
+			cnt += n / 5;
+			break;
+		}
+		
+		n -= 3;
+		cnt++;
+
+		if (n <= 0) {
+			break;
+		}
+
+	}
+	if (n < 0) {
+		printf("-1");
+	}
+	else printf("%d", cnt);
+	
+	return 0;
+}
