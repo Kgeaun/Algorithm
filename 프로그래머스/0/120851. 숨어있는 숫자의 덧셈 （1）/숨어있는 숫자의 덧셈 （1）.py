72d576d0e54a2640b5017bba9ca67e76a3a6a6f7
김가은 <128462535+Kgeaun@users.noreply.github.com>
2024-06-07 23:42:58 +0900

    Update 숨어있는 숫자의 덧셈 （1）.py

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
@@ -2,6 +2,6 @@ def solution(my_string):
     answer = 0
     
     for i in range(len(my_string)):
-        if my_string[i].isdigit( ):
+        if my_string[i].isdigit( ): # .isdigit( ) <- 문자가 정수인지 판별해줌
             answer += int(my_string[i])
     return answer
